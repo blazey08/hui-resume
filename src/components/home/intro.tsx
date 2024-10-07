1412ff020655160cd121ab82335b543d70f00182
blazey08 <lauyuhui18@yahoo.com.sg>
2024-10-07 20:01:49 +0800

    update content

--- src/components/home/intro.tsx
+++ src/components/home/intro.tsx
@@ -1,17 +1,23 @@
 import { Heading1 } from "../common/typography/Heading1";
 import { Heading2 } from "../common/typography/Heading2";
 import { Heading4 } from "../common/typography/Heading4";
-import styles from "@/styles/animation.module.css"
+import styles from "@/styles/animation.module.css";
+import { Heading5 } from "../common/typography/Heading5";
 
 export function Intro() {
-    return (
-        <div className={`text-left space-y-3 ${styles.slideIn}`}>
-            <Heading1>Hello, I'm Yu Hui</Heading1>
-            <Heading2>Software Developer | Crappy Gamer | Filler Term</Heading2>
-            <Heading4>Continue reading if... there's no if, just do it.</Heading4>
-            <Heading4>Please.</Heading4>
-            <Heading4>For Me.</Heading4>
-            <Heading4>For Humanity.</Heading4>
-        </div>
-    )
+  return (
+    <div className={`text-left space-y-3 ${styles.slideIn}`}>
+      <Heading1>Hello, I'm Yu Hui</Heading1>
+      <Heading2>
+        Software Developer | Casual Gamer | Entertainment Enthusiasist
+      </Heading2>
+      <Heading4>
+        I'm mainly just a developer, the rest are just filler terms
+      </Heading4>
+      <Heading4>Rule of three y'know?</Heading4>
+      <Heading5>
+        But I do like kpop and anime, so we could talk about that!
+      </Heading5>
+    </div>
+  );
 }
